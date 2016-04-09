916cc46b64e8b4173733da03c51ed305c084d097
Austin Keeley <austin.keeley@gmail.com>
2016-04-09 19:00:17 -0400

    now uses the session lib

--- client.js
+++ client.js
@@ -3,49 +3,16 @@ var fScraper = require('form-scraper');
 var pRequest = require('promisified-request');
 var cheerio = require('cheerio');
 
-request = request.defaults({
-  jar: true,
-});
-
-
-/*
- * Logs into pfSense and sets the global session ID
- */
-var login = function(hostname, username, password, cb) {
-  var promise = pRequest.create(request);
-  var formStructure = fScraper.fetchForm('#iform', 'http://' + hostname + '/index.php', promise);
-  fScraper.submitForm({
-    usernamefld: username,
-    passwordfld: password,
-    login: 'Login'
-  }, fScraper.provideForm(formStructure), promise).then(function(response) {
-    if (response.statusCode === 302) {
-      if (!response.headers['set-cookie']) {
-        cb('Cookie not set', null);
-      }
-      response.headers['set-cookie'].forEach(function(cookie) {
-        var pair = cookie.split(';');
-        var keyValue = pair[0].split('=');
-        if (keyValue[0] === 'PHPSESSID') {
-          cb(null, keyValue[1]);
-          found = true;
-          return;
-        }
-      });
-      if (!found) { cb('Cookie not set', null); }
-
-    }
-    else {
-      cb('Incorrect username or password', null);
-    }
-  });
-};
-
 /*
  * Gets the pfSense users
  */
-var getUsers = function(hostname, cb) {
-  request.get('http://' + hostname + '/system_usermanager.php', function(err, response) {
+var getUsers = function(session, cb) {
+  request.get({
+    url: 'http://' + session.hostname + '/system_usermanager.php',
+    headers: {
+      'Cookie': 'PHPSESSID=' + session.token
+    }
+  }, function(err, response) {
       var $ = cheerio.load(response.body);
       var results = [];
       var id = 0;
@@ -76,12 +43,18 @@ var getUsers = function(hostname, cb) {
 /*
  * Gets the certificates and private keys for a user
  */
-var getCerts = function(hostname, userID, cb) {
+var getCerts = function(session, userID, cb) {
 
-  var promise = pRequest.create(request);
-  var formStructure = fScraper.fetchForm('#iform2', 'http://' + hostname + '/system_usermanager.php', promise);
+  var r = request.defaults({
+    headers: {
+      'Cookie': 'PHPSESSID=' + session.token
+    }
+  });
+
+  var promise = pRequest.create(r);
+  var formStructure = fScraper.fetchForm('#iform2', 'http://' + session.hostname + '/system_usermanager.php', promise);
   formStructure.then(function(form) {
-    request.post('http://' + hostname + '/system_usermanager.php', {
+    r.post('http://' + session.hostname + '/system_usermanager.php', {
       form: {
         __csrf_magic: form.data.__csrf_magic,
         act: 'edit',
@@ -113,6 +86,9 @@ var getCerts = function(hostname, userID, cb) {
       cb(null, results);
     });
 
+  },
+  function(err) {
+    cb(err, null);
   });
 };
 
@@ -158,7 +134,6 @@ var exportKey = function(hostname, userID, certID, cb) {
 };
 
 module.exports = {
-  login: login,
   getUsers: getUsers,
   getCerts: getCerts,
   exportCert: exportCert,
